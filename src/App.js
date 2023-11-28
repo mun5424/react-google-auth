0cd5d08b8b271a052eaeadb0c930097836d3f872
mun5424 <mun5424@gmail.com>
2023-11-27 22:29:23 -0800

    google auth login with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React, { useState } from "react";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBjXcDHlOo90X5iBXc8udv9oCZTxWRlPAk",
-    authDomain: "react--auth-f1a25.firebaseapp.com",
-    projectId: "react--auth-f1a25",
-    storageBucket: "react--auth-f1a25.appspot.com",
-    messagingSenderId: "627008504533",
-    appId: "1:627008504533:web:1142cb324d2340e7c0185c"
-  };
-  
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const googleProvider = new firebase.auth.GoogleAuthProvider();
+import { auth, googleProvider } from './firebase.js';
 
 function App() {
   const [ loggedIn, setLoggedIn ] = useState(false);
